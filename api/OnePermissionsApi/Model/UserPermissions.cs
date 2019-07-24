164664531e8824797803bdd42a6c7a68fb938d7d
REDMOND\admirza <admirza@microsoft.com>
2019-07-24 13:10:46 -0700

    Changed names of files for easier usage in command line (ui and api) and restarted the UI project

---
+++ api/OnePermissionsApi/Model/UserPermissions.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace OnePermissionsApi.Model
+{
+    public class UserPermissions
+    {
+        [JsonProperty(PropertyName = "alias")]
+        public string Alias { get; set; }
+
+        [JsonProperty(PropertyName = "first_name")]
+        public string FirstName { get; set; }
+
+        [JsonProperty(PropertyName = "last_name")]
+        public string LastName { get; set; }
+
+        [JsonProperty(PropertyName = "permissions")]
+        public List<Permissions> Permissions { get; set; }
+
+        [JsonProperty(PropertyName = "success")]
+        public bool Success { get; set; }
+    }
+}
