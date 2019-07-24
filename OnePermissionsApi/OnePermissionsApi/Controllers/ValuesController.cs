8ff19004459898432f3e05ddb24bb04679d105c6
Adam Mirza <adammirza12@hotmail.com>
2019-07-24 11:36:02 -0700

    Made working API with framework for response and email regex

---
+++ OnePermissionsApi/OnePermissionsApi/Controllers/ValuesController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Mail;
+using Microsoft.AspNetCore.Mvc;
+using OnePermissionsApi.Model;
+
+namespace OnePermissionsApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{alias}")]
+        public ActionResult<UserPermissions> Get(string alias)
+        {
+            // Check valid email
+            try
+            {
+                MailAddress m = new MailAddress(alias);
+                if (m.Host.ToLower() != "microsoft.com")
+                {
+                    throw new FormatException("Not an alias");
+                }
+
+            }
+            catch (FormatException)
+            {
+                return new UserPermissions()
+                {
+                    Success = false
+                };
+            }
+
+            return new UserPermissions()
+            {
+                Alias = alias,
+                Success = true
+            };
+        }
+
+        //// POST api/values
+        //[HttpPost]
+        //public void Post([FromBody] string value)
+        //{
+        //}
+
+        //// PUT api/values/5
+        //[HttpPut("{id}")]
+        //public void Put(int id, [FromBody] string value)
+        //{
+        //}
+
+        //// DELETE api/values/5
+        //[HttpDelete("{id}")]
+        //public void Delete(int id)
+        //{
+        //}
+    }
+}
---
+++ OnePermissionsApi/OnePermissionsApi/Model/Permissions.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+
+namespace OnePermissionsApi.Model
+{
+    public class Permissions
+    {
+        [JsonProperty(PropertyName = "name")]
+        public string Name { get; set; }
+
+        [JsonProperty(PropertyName = "type")]
+        public string Type { get; set; }
+
+        [JsonProperty(PropertyName = "role")]
+        public string Role { get; set; }
+
+        [JsonProperty(PropertyName = "contact")]
+        public string Contact { get; set; }
+    }
+}
